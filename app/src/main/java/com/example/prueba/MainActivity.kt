9fdd74c3474c1699e22fe030cf25d070ed89facd
Esteban <estbansds@gmail.com>
2025-03-10 19:24:31 -0600

    prueba de array

--- app/src/main/java/com/example/prueba/MainActivity.kt
+++ app/src/main/java/com/example/prueba/MainActivity.kt
@@ -2,12 +2,8 @@ package com.example.prueba
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.EditText
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.prueba.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +20,25 @@ class MainActivity : AppCompatActivity() {
         /*val product : String = findViewById<EditText>(R.id.et_producto).text.toString()
         findViewById<TextView>(R.id.tv_titulo).text = product
         val message : String = findViewById<TextView>(R.id.tv_titulo).text.toString()*/
+
+        val myArray = arrayOf("jabon","pasta","sacate","shampo","espejo")
+        Log.e("prueba1", myArray[4].toString())
+        myArray[4] = "papel"
+        Log.e("prueba1", myArray[4].toString())
+        Log.e("prueba1", myArray[4].first().toString())
+        Log.e("prueba1", myArray.first().toString())
+        Log.e("prueba1", myArray[4].last().toString())
+        Log.e("prueba1", myArray.last().toString())
+
+        myArray.forEach {
+            if(it.first()=='j'){
+                Log.e("prueba1", "encontre la j")
+            } else{
+                Log.e("prueba1", "este valor no tiene j")
+            }
+
+        }
+
 
 
         binding.btnGuardar.setOnClickListener {
